921b25af32d7c3422e3ab657c4b04c59b9e69832
canisin <canisin@users.noreply.github.com>
2020-02-17 18:15:01 +0100

    - Added Honzor players.

---
+++ HonStatsManager/Honzor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace HonStatsManager
+{
+    internal static class Honzor
+    {
+        [SuppressMessage("ReSharper", "StringLiteralTypo")]
+        public static List<Player> Players
+            = new List<Player>
+            {
+                new Player("1220342", "budrick", "Can"),
+                new Player("1248746", "IfUCanUCan", "Baran"),
+                new Player("2997424", "sbarcan", "Barış"),
+                new Player("1260616", "papercute", "Batu"),
+                new Player("1248693", "Turritopsis", "Gökhan")
+            };
+    }
+}
---
+++ HonStatsManager/Player.cs
@@ -0,0 +1,16 @@
+﻿namespace HonStatsManager
+{
+    internal class Player
+    {
+        public readonly string AccountId;
+        public readonly string Nickname;
+        public readonly string Name;
+
+        public Player(string accountId, string nickname, string name)
+        {
+            AccountId = accountId;
+            Nickname = nickname;
+            Name = name;
+        }
+    }
+}
