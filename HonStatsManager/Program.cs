7d35833e10c00ee35a4ca30ba43bbe7ddf1571f4
canisin <canisin@users.noreply.github.com>
2020-02-17 18:40:09 +0100

    - Get last match in main.

--- HonStatsManager/Program.cs
+++ HonStatsManager/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Newtonsoft.Json;
 
 namespace HonStatsManager
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            try
+            {
+                MainImpl(args);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+            Console.WriteLine("Please press any key to continue...");
+            Console.ReadKey(true);
+        }
+
+        private static void MainImpl(string[] args)
+        {
+            Console.WriteLine(HonApi.GetMatchRaw(HonApi.GetMatchHistory(Honzor.Players[0]).Last()));
         }
     }
 }
